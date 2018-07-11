6636f0a628a02d78246e1d0578495dfd5d2449ee
windofred <1342670786@qq.com>
2018-07-11 22:51:13 +0800

    Update InterfaceController.java

--- src/main/java/com/migu/spms/controller/InterfaceController.java
+++ src/main/java/com/migu/spms/controller/InterfaceController.java
@@ -30,6 +30,7 @@ public class InterfaceController {
 
     @RequestMapping(value = "/addInterface", method = RequestMethod.POST)
     public String addInterface(Model model, InterfaceDO interfaceDO) {
+        System.out.println();
         if (interfaceDO != null) {
             interfaceService.saveInterface(interfaceDO);
         }
